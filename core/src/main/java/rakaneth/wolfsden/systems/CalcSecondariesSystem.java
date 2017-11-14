7a91e50473d0c629deb5cd1fbcdd864f008a743d
Quincy West <rakaneth@gmail.com>
2017-11-14 10:52:46 -0500

    same as above

--- core/src/main/java/rakaneth/wolfsden/systems/CalcSecondariesSystem.java
+++ core/src/main/java/rakaneth/wolfsden/systems/CalcSecondariesSystem.java
@@ -4,9 +4,14 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
 
+import rakaneth.wolfsden.RKDice;
+import rakaneth.wolfsden.components.Armor;
+import rakaneth.wolfsden.components.Mainhand;
 import rakaneth.wolfsden.components.Mapper;
+import rakaneth.wolfsden.components.Offhand;
 import rakaneth.wolfsden.components.SecondaryStats;
 import rakaneth.wolfsden.components.Stats;
+import rakaneth.wolfsden.components.Trinket;
 
 public class CalcSecondariesSystem extends IteratingSystem
 {
@@ -23,11 +28,24 @@ public class CalcSecondariesSystem extends IteratingSystem
 		Stats stats = Mapper.stats.get(entity);
 		SecondaryStats secStats = Mapper.secondaries.get(entity);
 		
+		Armor arm = Mapper.armors.get(entity);
+		Trinket trink = Mapper.trinkets.get(entity);
+		Mainhand mh = Mapper.mainhands.get(entity);
+		Offhand oh = Mapper.offhands.get(entity);
+		
+		RKDice totalAtk = RKDice.add(arm.atk, trink.atk, mh.atk, oh.atk);
+		RKDice totalDmg = RKDice.add(arm.dmg, trink.dmg, mh.dmg, oh.dmg);
+		int totalDef = arm.def + trink.def + mh.def + oh.def;
+		
+
+		
 		secStats.moveDelay = Math.max(1, 11 - stats.spd);
 		secStats.atkDelay = Math.max(1, 11 - stats.skl);
 		secStats.atk.set(stats.skl, stats.skl);
 		secStats.def = stats.spd;
 		secStats.dmg.set(stats.str, stats.str);
 	}
+	
+
 
 }
