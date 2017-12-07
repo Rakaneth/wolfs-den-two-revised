40090a083b214a82e16d6fca34d501556b16617e
Quincy West <rakaneth@gmail.com>
2017-12-07 09:38:58 -0500

    generalize btrees, implement CallOffLackeysTask

---
+++ core/src/main/java/rakaneth/wolfsden/ai/tasks/CallOffLackeysTask.java
@@ -0,0 +1,32 @@
+package rakaneth.wolfsden.ai.tasks;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.ai.btree.LeafTask;
+import com.badlogic.gdx.ai.btree.Task;
+
+import rakaneth.wolfsden.FactionManager;
+import rakaneth.wolfsden.WolfUtils;
+import rakaneth.wolfsden.components.Mapper;
+
+public class CallOffLackeysTask extends LeafTask<Entity>
+{
+
+  @Override
+  public Status execute()
+  {
+    Entity entity = getObject();
+    for (Entity lackey: FactionManager.instance.allTeammates(entity))
+    {
+      WolfUtils.log("AI", "%s calls off %s", Mapper.getID(entity), Mapper.getID(lackey));
+      Mapper.ai.get(lackey).clearTarget();
+    }
+    return Status.SUCCEEDED;
+  }
+
+  @Override
+  protected Task<Entity> copyTo(Task<Entity> task)
+  {
+    return task;
+  }
+  
+}
